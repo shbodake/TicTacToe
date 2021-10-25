4a143f3f8604faccfe97b6a25522fda5e38aace7
shbodake <sayalibodake28@gmail.com>
2021-10-25 19:52:45 +0530

    Added: Assign the inputs

--- src/com/blp/tictactoe/TicTacToeGame.java
+++ src/com/blp/tictactoe/TicTacToeGame.java
@@ -9,9 +9,29 @@ public class TicTacToeGame {
             board[i] = ' ';
         }
     }
-
+    public static void assignInputs()
+    {
+        System.out.println("Choose your choice between X or O");
+        Scanner scanner = new Scanner(System.in);
+        char playerchoice = scanner.next().charAt(0);
+        if(playerchoice == 'O')
+        {
+            System.out.println("Player choice is: " +playerchoice);
+            System.out.println("Computer choice is X");
+        }else if (playerchoice == 'X')
+        {
+            System.out.println("Player choice is: " +playerchoice);
+            System.out.println("Computer choice is O");
+        }
+        else
+        {
+            System.out.println("Enter choice is not correct then enter correct choice");
+            assignInputs();
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
         createBoard();
+        assignInputs();
     }
 }
