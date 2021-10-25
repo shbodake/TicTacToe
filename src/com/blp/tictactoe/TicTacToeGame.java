6a175024bfe685b921f9ae6f04fe853a2a571649
shbodake <sayalibodake28@gmail.com>
2021-10-25 19:39:39 +0530

    Added: creat the board

--- src/com/blp/tictactoe/TicTacToeGame.java
+++ src/com/blp/tictactoe/TicTacToeGame.java
@@ -1,4 +1,17 @@
 package com.blp.tictactoe;
-
+import java.util.Scanner;
 public class TicTacToeGame {
+    public static char[] board = new char[10];
+    public static void createBoard()
+    {
+        for(int i = 1; i <board.length; i++)
+        {
+            board[i] = ' ';
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tic Tac Toe Game");
+        createBoard();
+    }
 }
