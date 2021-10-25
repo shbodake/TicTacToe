7796568964e152cff88453872172a784e8c3e944
shbodake <sayalibodake28@gmail.com>
2021-10-25 20:13:30 +0530

    Added: Show the board

--- src/com/blp/tictactoe/TicTacToeGame.java
+++ src/com/blp/tictactoe/TicTacToeGame.java
@@ -1,6 +1,7 @@
 package com.blp.tictactoe;
 import java.util.Scanner;
 public class TicTacToeGame {
+    Scanner scanner = new Scanner(System.in);
     public static char[] board = new char[10];
     public static void createBoard()
     {
@@ -29,9 +30,20 @@ public class TicTacToeGame {
             assignInputs();
         }
     }
+    public static void showBoard()
+    {
+        System.out.println("|-----------|");
+        System.out.println("| "+board[1]+ " | " +board[2]+ " | " +board[3]+ " |");
+        System.out.println("|-------|");
+        System.out.println("| "+board[4]+ " | " +board[5]+ " | " +board[6]+ " |");
+        System.out.println("|-----------|");
+        System.out.println("| "+board[7]+ " | " +board[8]+ " | " +board[9]+ " |");
+        System.out.println("|--------|");
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
         createBoard();
         assignInputs();
+        showBoard();
     }
 }
